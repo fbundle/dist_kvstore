bd10c1a1f17e89e5116cb1b9389775b5cbd729e9
khanh <khanh.nguyen.contact@gmail.com>
2025-06-14 02:26:47 +0800

    auto_push

--- kvstore/kvstore.go
+++ kvstore/kvstore.go
@@ -4,7 +4,13 @@ import "sync"
 
 // Store - supposed to be threadsafe for each key and persistent
 type Store[K comparable, V any] interface {
-	Update(k K, update func(V) V)
+	Update(update func(txn Txn[K, V]) any) any
+}
+
+type Txn[K comparable, V any] interface {
+	Get(k K) (v V, ok bool)
+	Set(k K, v V)
+	Del(k K)
 }
 
 func NewMemStore[K comparable, V any]() Store[K, V] {
@@ -14,22 +20,26 @@ func NewMemStore[K comparable, V any]() Store[K, V] {
 	}
 }
 
-func zero[T any]() T {
-	var v T
-	return v
-}
-
 type memStore[K comparable, V any] struct {
 	mu    sync.Mutex
 	store map[K]V
 }
 
-func (m *memStore[K, V]) Update(k K, update func(V) V) {
+func (m *memStore[K, V]) Update(update func(txn Txn[K, V]) any) any {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	v, ok := m.store[k]
-	if !ok {
-		v = zero[V]()
-	}
-	m.store[k] = update(v)
+	return update(m)
+}
+
+func (m *memStore[K, V]) Get(k K) (v V, ok bool) {
+	v, ok = m.store[k]
+	return v, ok
+}
+
+func (m *memStore[K, V]) Set(k K, v V) {
+	m.store[k] = v
+}
+
+func (m *memStore[K, V]) Del(k K) {
+	delete(m.store, k)
 }
--- paxos/simple_acceptor.go
+++ paxos/simple_acceptor.go
@@ -5,6 +5,7 @@ import "paxos/kvstore"
 type ProposalNumber uint64
 
 const (
+	ZERO     ProposalNumber = 0
 	COMMITED ProposalNumber = 18446744073709551615
 )
 
@@ -22,61 +23,69 @@ type simpleAcceptor struct {
 	log kvstore.Store[LogId, Promise]
 }
 
-func (a *simpleAcceptor) get(logId LogId) Promise {
-	var promise Promise
-	a.log.Update(logId, func(p Promise) Promise {
-		promise = p
-		return p
-	})
-	return promise
+func getOrSetLogEntry(txn kvstore.Txn[LogId, Promise], logId LogId) (p Promise) {
+	if v, ok := txn.Get(logId); ok {
+		return v
+	}
+	v := Promise{
+		Proposal: ZERO,
+		Value:    nil,
+	}
+	txn.Set(logId, v)
+	return v
+}
+
+func (a *simpleAcceptor) get(logId LogId) (promise Promise) {
+	return a.log.Update(func(txn kvstore.Txn[LogId, Promise]) any {
+		return getOrSetLogEntry(txn, logId)
+	}).(Promise)
+
 }
 
 func (a *simpleAcceptor) commit(logId LogId, v Value) {
-	a.log.Update(logId, func(_ Promise) Promise {
-		return Promise{
+	a.log.Update(func(txn kvstore.Txn[LogId, Promise]) any {
+		txn.Set(logId, Promise{
 			Proposal: COMMITED,
 			Value:    v,
-		}
+		})
+		return nil
 	})
 }
 
 func (a *simpleAcceptor) prepare(logId LogId, proposal ProposalNumber) (proposalOut ProposalNumber, ok bool) {
-	a.log.Update(logId, func(p Promise) Promise {
-		if p.Proposal == COMMITED {
-			// reject if committed
-			proposalOut, ok = COMMITED, false
-			return p
+	r := a.log.Update(func(txn kvstore.Txn[LogId, Promise]) any {
+		p := getOrSetLogEntry(txn, logId)
+		if p.Proposal == COMMITED { // reject if committed
+			return [2]any{COMMITED, false}
 		}
-		if proposal <= p.Proposal {
-			// fulfill promise
-			proposalOut, ok = p.Proposal, false
-			return p
+		if proposal <= p.Proposal { // fulfill promise
+			return [2]any{p.Proposal, false}
 		}
-		// make promise
-		p.Proposal = proposal
-		proposalOut, ok = proposal, true
-		return p
-	})
-	return
+		// make new promise
+		txn.Set(logId, Promise{
+			Proposal: proposal,
+			Value:    p.Value, // old value
+		})
+		return [2]any{proposal, true}
+	}).([2]any)
+	return r[0].(ProposalNumber), r[1].(bool)
 }
 
 func (a *simpleAcceptor) accept(logId LogId, proposal ProposalNumber, value Value) (proposalOut ProposalNumber, ok bool) {
-	a.log.Update(logId, func(p Promise) Promise {
-		if p.Proposal == COMMITED {
-			// reject if committed
-			proposalOut, ok = COMMITED, false
-			return p
+	r := a.log.Update(func(txn kvstore.Txn[LogId, Promise]) any {
+		p := getOrSetLogEntry(txn, logId)
+		if p.Proposal == COMMITED { // reject if committed
+			return [2]any{COMMITED, false}
 		}
-		if proposal < p.Proposal {
-			// fulfill promise
-			proposalOut, ok = p.Proposal, false
-			return p
+		if proposal < p.Proposal { // fulfill promise
+			return [2]any{p.Proposal, false}
 		}
-		// make promise
-		p.Proposal = proposal
-		p.Value = value
-		proposalOut, ok = proposal, true
-		return p
-	})
-	return
+		// make new promise
+		txn.Set(logId, Promise{
+			Proposal: proposal,
+			Value:    value, // new value
+		})
+		return [2]any{proposal, true}
+	}).([2]any)
+	return r[0].(ProposalNumber), r[1].(bool)
 }
