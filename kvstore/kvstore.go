e8a482a8f5702f727d98db68ddbd20a051011879
khanh <khanh.nguyen.contact@gmail.com>
2025-06-14 00:05:21 +0800

    auto_push

--- kvstore/kvstore.go
+++ kvstore/kvstore.go
@@ -3,9 +3,6 @@ package kvstore
 import "sync"
 
 type Store[K comparable, V any] interface {
-	Get(K) (V, bool)
-	Set(K, V)
-	Del(K)
 	Update(k K, update func(V) V)
 }
 
@@ -14,16 +11,11 @@ func zero[T any]() T {
 	return v
 }
 
+// TODO - implement other persistent stores
+
 type memStore[K comparable, V any] struct {
 	mu    sync.Mutex
 	store map[K]V
-}
-
-func (m *memStore[K, V]) Get(k K) (V, bool) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	v, ok := m.store[k]
-	return v, ok
 }
 
 func (m *memStore[K, V]) Update(k K, update func(V) V) {
@@ -34,18 +26,6 @@ func (m *memStore[K, V]) Update(k K, update func(V) V) {
 		v = zero[V]()
 	}
 	m.store[k] = update(v)
-}
-
-func (m *memStore[K, V]) Set(k K, v V) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.store[k] = v
-}
-
-func (m *memStore[K, V]) Del(k K) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	delete(m.store, k)
 }
 
 func NewMemStore[K comparable, V any]() Store[K, V] {
