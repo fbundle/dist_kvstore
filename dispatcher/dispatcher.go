08fe7b40ee7d0111962fea0221e55bf45c6cb2a6
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-06-16 15:36:17 +0800

    auto_push

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -6,112 +6,47 @@ import (
 	"reflect"
 )
 
-type Dispatcher struct {
-	handlers map[string]handlerEntry
-}
-
-type handlerEntry struct {
+type handler struct {
 	handlerFunc reflect.Value
 	argType     reflect.Type
 }
 
-func NewDispatcher() *Dispatcher {
-	return &Dispatcher{handlers: make(map[string]handlerEntry)}
+type Dispatcher struct {
+	handlerMap map[string]handler
 }
 
-func (d *Dispatcher) AddHandler(name string, handler any) {
-	hv := reflect.ValueOf(handler)
-	ht := hv.Type()
-	if ht.Kind() != reflect.Func || ht.NumIn() != 1 || ht.NumOut() != 1 {
-		panic("handler must be of form func(*SomeRequest) *SomeResponse")
-	}
-	arg := ht.In(0)
-	if arg.Kind() != reflect.Ptr || ht.Out(0).Kind() != reflect.Ptr {
-		panic("handler arguments and return type must be pointers")
-	}
-	d.handlers[name] = handlerEntry{
-		handlerFunc: hv,
-		argType:     arg,
+func NewDispatcher() *Dispatcher {
+	return &Dispatcher{
+		handlerMap: make(map[string]handler),
 	}
 }
 
 type Message struct {
-	Type string          `json:"type"`
-	Body json.RawMessage `json:"body"`
+	Name string `json:"name"`
+	Body []byte `json:"body"`
 }
 
-func (d *Dispatcher) Handle(data []byte) []byte {
-	var msg Message
-	if err := json.Unmarshal(data, &msg); err != nil {
-		return marshalError(err)
+func (d *Dispatcher) Handle(input []byte) (output []byte, err error) {
+	msg := Message{}
+	if err := json.Unmarshal(input, &msg); err != nil {
+		return nil, err
 	}
 
-	entry, ok := d.handlers[msg.Type]
+	h, ok := d.handlerMap[msg.Name]
 	if !ok {
-		return marshalError(fmt.Errorf("unknown message type: %s", msg.Type))
+		return nil, fmt.Errorf("command not found")
 	}
 
-	argPtr := reflect.New(entry.argType.Elem()).Interface()
+	argPtr := reflect.New(h.argType.Elem()).Interface()
 	if err := json.Unmarshal(msg.Body, argPtr); err != nil {
-		return marshalError(err)
+		return nil, err
 	}
 
-	res := entry.handlerFunc.Call([]reflect.Value{reflect.ValueOf(argPtr)})[0].Interface()
+	out := h.handlerFunc.Call([]reflect.Value{reflect.ValueOf(argPtr)})[0].Interface()
 
-	output, err := json.Marshal(res)
+	output, err = json.Marshal(out)
 	if err != nil {
-		return marshalError(err)
+		return nil, err
 	}
-	return output
-}
-
-func marshalError(err error) []byte {
-	msg := map[string]string{"error": err.Error()}
-	b, _ := json.Marshal(msg)
-	return b
-}
-
-type TransportFunc func(data []byte) ([]byte, error)
-
-func RemoteCall[Req any, Res any](name string, req Req, transport TransportFunc) (Res, error) {
-	var zero Res
-
-	// Type name from struct
-	t := reflect.TypeOf(req)
-	if t.Kind() != reflect.Ptr {
-		return zero, fmt.Errorf("req must be a pointer")
-	}
-
-	body, err := json.Marshal(req)
-	if err != nil {
-		return zero, err
-	}
-	msg := Message{
-		Type: name,
-		Body: body,
-	}
-	msgBytes, err := json.Marshal(msg)
-	if err != nil {
-		return zero, err
-	}
-
-	// 🔌 Send over the transport
-	resBytes, err := transport(msgBytes)
-	if err != nil {
-		return zero, err
-	}
-
-	// Decode response
-	var res Res
-	if err := json.Unmarshal(resBytes, &res); err != nil {
-		// Try to parse error message
-		var errMsg map[string]string
-		if json.Unmarshal(resBytes, &errMsg) == nil {
-			if msg, ok := errMsg["error"]; ok {
-				return zero, fmt.Errorf(msg)
-			}
-		}
-		return zero, err
-	}
-	return res, nil
+	return output, nil
 }
