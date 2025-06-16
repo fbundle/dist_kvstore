ec706fc0dabc552e83f2e0cd86540169ddf9bd0c
khanh <khanh.nguyen.contact@gmail.com>
2025-06-16 23:36:35 +0800

    auto_push

--- client.py
+++ client.py
@@ -4,35 +4,27 @@ from typing import Iterator
 
 import requests
 
-type HTTPError = requests.Response
+def make_request(method: str, addr: str, path: str, **kwargs) -> requests.Response:
+    res = requests.request(method, f"{addr}/{path}", **kwargs)
+    res.raise_for_status()
+    return res
 
 class KVStore:
     def __init__(self, addr: str = "http://localhost:4000"):
         self.addr = addr
 
     def __getitem__(self, key: str) -> str:
-        res = requests.get(f"{self.addr}/kvstore/{key}")
-        res.raise_for_status()
-        return res.text
+        return make_request("GET", self.addr, f"kvstore/{key}").text
 
     def next_token(self) -> int:
-        res = requests.get(f"{self.addr}/kvstore_next")
-        res.raise_for_status()
-        return int(res.text)
+        return int(make_request("GET", self.addr, "kvstore_next").text)
 
 
     def set(self, token: int, key: str, value: str = ""):
-        res = requests.post(f"{self.addr}/kvstore/{key}?token={token}", data=value)
-        res.raise_for_status()
-
-    def __delitem__(self, token: int, key: str):
-        return self.set(token, key, "")
-
+        return make_request("PUT", self.addr, f"kvstore/{key}?token={token}", data=value)
 
     def keys(self) -> list[str]:
-        res = requests.get(f"{self.addr}/kvstore_keys")
-        res.raise_for_status()
-        return json.loads(res.text)
+        return json.loads(make_request("GET", self.addr, "kvstore_keys").text)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(keys:{self.keys()})"
