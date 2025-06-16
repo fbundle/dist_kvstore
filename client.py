f25aff128702f625280bb66f6efddf2aac87d73a
khanh <khanh.nguyen.contact@gmail.com>
2025-06-16 21:19:47 +0800

    auto_push

--- client.py
+++ client.py
@@ -5,12 +5,17 @@ import requests
 class KVStore:
     def __init__(self, addr: str = "http://localhost:4000/kvstore"):
         self.addr = addr
+        self.keys = []
 
-    def get(self, key: str) -> str:
+    def __getitem__(self, key: str) -> str:
         return requests.get(f"{self.addr}/{key}").text
 
-    def set(self, key: str, value: str = ""):
+    def __setitem__(self, key: str, value: str = ""):
         return requests.post(f"{self.addr}/{key}", data=value).text
+
+    def __delitem__(self, key: str):
+        return self.__setitem__(key, "")
+
 
     def keys(self) -> list[str]:
         text = requests.get(f"{self.addr}/").text
