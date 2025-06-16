b1a3045a69d0b5341fa12ff00961f3aa5aadb3bb
khanh <khanh.nguyen.contact@gmail.com>
2025-06-17 01:53:02 +0800

    auto_push

--- cmd/cat/main.go
+++ cmd/cat/main.go
@@ -8,48 +8,53 @@ import (
 )
 
 func main() {
-	badgerPath := os.Args[1]
-	db, err := badger.Open(badger.DefaultOptions(badgerPath))
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	for i := 1; i < len(os.Args); i++ {
+		badgerPath := os.Args[i]
+		func(badgerPath string) {
+			db, err := badger.Open(badger.DefaultOptions(badgerPath))
+			if err != nil {
+				panic(err)
+			}
+			defer db.Close()
 
-	log := make(map[int]string)
-	err = db.View(func(txn *badger.Txn) error {
-		opts := badger.DefaultIteratorOptions
-		it := txn.NewIterator(opts)
-		defer it.Close()
-		for it.Rewind(); it.Valid(); it.Next() {
-			item := it.Item()
-			keyBytes := item.Key()
-			err := item.Value(func(valBytes []byte) error {
-				key, err := strconv.Atoi(string(keyBytes))
-				if err != nil {
-					fmt.Println(err)
-					return nil
+			log := make(map[int]string)
+			err = db.View(func(txn *badger.Txn) error {
+				opts := badger.DefaultIteratorOptions
+				it := txn.NewIterator(opts)
+				defer it.Close()
+				for it.Rewind(); it.Valid(); it.Next() {
+					item := it.Item()
+					keyBytes := item.Key()
+					err := item.Value(func(valBytes []byte) error {
+						key, err := strconv.Atoi(string(keyBytes))
+						if err != nil {
+							fmt.Println(err)
+							return nil
+						}
+						val := string(valBytes)
+						log[key] = val
+						return nil
+					})
+					if err != nil {
+						return err
+					}
 				}
-				val := string(valBytes)
-				log[key] = val
 				return nil
 			})
 			if err != nil {
-				return err
+				panic(err)
 			}
-		}
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-	key := 0
-	for {
-		val, ok := log[key]
-		if !ok {
-			fmt.Printf("stopped with %d keys left\n", len(log))
-			break
-		}
-		fmt.Printf("%d: %s\n", key, val)
-		key++
+			key := 0
+			for {
+				val, ok := log[key]
+				if !ok {
+					_, _ = fmt.Fprintf(os.Stderr, "stopped with %d keys left\n", len(log))
+					break
+				}
+				fmt.Printf("%d: %s\n", key, val)
+				delete(log, key)
+				key++
+			}
+		}(badgerPath)
 	}
 }
