31ea4493431636d307e4b1a828fd1f07fe4bf520
khanh <khanh.nguyen.contact@gmail.com>
2025-06-16 21:12:32 +0800

    auto_push

--- client.py
+++ client.py
@@ -1,2 +1,17 @@
+import json
+
 import requests
 
+class KVStore:
+    def __init__(self, addr: str):
+        self.addr = addr
+
+    def get(self, key: str) -> str:
+        return requests.get(f"{self.addr}/{key}").text
+
+    def set(self, key: str, value: str = ""):
+        return requests.post(f'{self.addr}/{key}', data=value).text
+
+    def keys(self) -> list[str]:
+        text = requests.get(f"{self.addr}/").text
+        return json.loads(text)
