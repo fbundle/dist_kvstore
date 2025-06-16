50f4938bdf79699fd0b979ccfa85900531a08537
khanh <khanh.nguyen.contact@gmail.com>
2025-06-16 21:22:46 +0800

    auto_push

--- client.py
+++ client.py
@@ -5,18 +5,28 @@ import requests
 class KVStore:
     def __init__(self, addr: str = "http://localhost:4000/kvstore"):
         self.addr = addr
-        self.keys = []
 
     def __getitem__(self, key: str) -> str:
-        return requests.get(f"{self.addr}/{key}").text
+        res = requests.get(f"{self.addr}/{key}")
+        if res.status_code != 200:
+            print(f"Error: {res.text}")
+            return ""
+        return res.text
 
     def __setitem__(self, key: str, value: str = ""):
-        return requests.post(f"{self.addr}/{key}", data=value).text
+        res = requests.post(f"{self.addr}/{key}", data=value)
+        if res.status_code != 200:
+            print(f"Error: {res.text}")
+            return ""
+        return res.text
 
     def __delitem__(self, key: str):
         return self.__setitem__(key, "")
 
 
     def keys(self) -> list[str]:
-        text = requests.get(f"{self.addr}/").text
-        return json.loads(text)
+        res = requests.get(f"{self.addr}/")
+        if res.status_code != 200:
+            print(f"Error: {res.text}")
+            return []
+        return json.loads(res.text)
