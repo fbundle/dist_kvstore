677a33e9d10eca51174f0f513d10f0f5aa802f15
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-06-19 17:43:20 +0800

    auto_push

---
+++ dist_kvstore/generate_config.py
@@ -0,0 +1,22 @@
+import sys
+
+if __name__ == "__main__":
+    app_name: str = sys.argv[1]
+    host_list: list[str] = sys.argv[2].split(",")
+    private_key: str = sys.argv[3]
+
+    assert app_name == "kvstore"
+
+    config = {
+        "private_key": private_key,
+        "host_list": []
+    }
+
+    for host in host_list:
+        config["host_list"].append({
+            "badger": f"data/acceptor_{host}",
+            "rpc": f"{host}:13800",
+            "store": "localhost:4000",
+        })
+    
+    print(config, file=sys.stdout)
