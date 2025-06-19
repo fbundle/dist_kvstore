5c096ef1a92157482310abca4fde9c1e957bfdb0
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-06-19 14:25:21 +0800

    auto_push

--- cmd/util/make_script.py
+++ cmd/util/make_script.py
@@ -8,7 +8,7 @@ TMP_DIR = "tmp"
 CONFIG_PATH = f"{TMP_DIR}/config.json"
 RUN_PATH = f"{TMP_DIR}/run_all.sh"
 STOP_PATH = f"{TMP_DIR}/stop_all.sh"
-GO_PATH = "go/bin/go"
+GO_PATH = "golang/go/bin/go"
 TMUX_SESSION = "kvstore"
 AES_KEY = "AES_KEY"
 
