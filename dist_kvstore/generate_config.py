73c63f36458910567e920f69b36035d3c598e000
nguyen_ngoc_khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2025-06-19 17:46:07 +0800

    auto_push

--- dist_kvstore/generate_config.py
+++ dist_kvstore/generate_config.py
@@ -1,11 +1,12 @@
 import sys
+import json
 
 if __name__ == "__main__":
     app_name: str = sys.argv[1]
     host_list: list[str] = sys.argv[2].split(",")
     private_key: str = sys.argv[3]
 
-    assert app_name == "kvstore"
+    assert app_name == "dist_kvstore"
 
     config = {
         "private_key": private_key,
@@ -19,4 +20,4 @@ if __name__ == "__main__":
             "store": "localhost:4000",
         })
     
-    print(config, file=sys.stdout)
+    print(json.dumps(config, indent=2), file=sys.stdout)
