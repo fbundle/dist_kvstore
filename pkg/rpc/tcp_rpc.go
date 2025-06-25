02e65e0ed6d64d3275a7b0f9082aab6d63e485ea
khanh <khanh.nguyen.contact@gmail.com>
2025-06-25 11:41:53 +0800

    auto_push

--- pkg/rpc/tcp_rpc.go
+++ pkg/rpc/tcp_rpc.go
@@ -17,8 +17,8 @@ const (
 
 type TCPServer interface {
 	Handle(input []byte) (output []byte, err error)
-	ListenAndServe() error
 	Register(cmd string, h any) TCPServer
+	ListenAndServe() error
 	Close() error
 }
 
